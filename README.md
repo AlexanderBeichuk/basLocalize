7f9fd041160673fdfe29e584cdc4664a2984a5d5
Alex Beichuk <alexander.beichuk@gmail.com>
2018-06-13 20:11:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+How use:
+Include this module in your application module:
+```js
+var app = angular.module('appModule', [
+    'basLocalize'
+]);
+```
+For get current language:
+```js
+var language = localStorage.getItem("language") || 'en';
+```
+
+We have variable for each languages with content:
+```js
+window.translations.cn = {
+    "eth": {
+        "collected": "筹募的ETH "
+    }
+};
+```
+
+Use your variable in
+```js
+basLocalizeService.changeLanguage(currentLanguage)
+```
+in controller for change language:
+```js
+app.controller('homeController', [ '$scope', 'basLocalizeService', function ($scope, basLocalizeService) {
+    $scope.changeLanguage = function(currentLanguage) {  //currentLanguage = cn
+        basLocalizeService.changeLanguage(currentLanguage);
+            };
+    }])
+```
+
+
+Use in file.html:
+```html
+<span ng-localize="eth.collected">template text</span>
+```
+
+GOOD LUCK!
---
+++ localize.module.js
@@ -0,0 +1,37 @@
+var basLocalizeModule = angular.module('basLocalize', []);
+
+basLocalizeModule.directive("ngLocalize", function() {
+   return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+
+         var language = localStorage.getItem("language") || 'en';
+
+         function resolveObject(path, obj) {
+            return path.split('.').reduce(function(prev, curr) {
+               return prev ? prev[curr] : undefined
+            }, obj || self);
+         };
+
+         element.html(resolveObject(attrs.ngLocalize, window.translations[language]));
+
+         scope.$on('baslanguage:change', function (event, data) {
+            element.html(resolveObject(attrs.ngLocalize, window.translations[data]));
+         });
+
+         scope.$watch(attrs.ngLocalize, function (path) {
+            element.html(resolveObject(path, window.translations[localStorage.getItem("language") || 'en']));
+         });
+
+      }
+   };
+});
+
+basLocalizeModule.factory('basLocalizeService', ['$rootScope', function ($rootScope) {
+   return {
+      changeLanguage: function (language) {
+         $rootScope.$broadcast('baslanguage:change', language);
+      }
+   }
+
+}]);
